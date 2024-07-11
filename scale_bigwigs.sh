2ac8b7e81679c2d3f083ffa502a0a71bb49bb5ee
schoo7 <111924574+schoo7@users.noreply.github.com>
2024-07-11 10:25:12 -0500

    Add files via upload

---
+++ scale_bigwigs.sh
@@ -0,0 +1,70 @@
+bigwigAverage -b E11_1_1.bigWig --scaleFactors 2 -o E11_1_1.bw --numberOfProcessors max
+bigwigAverage -b E12_1.bigWig --scaleFactors 2 -o E12_1.bw --numberOfProcessors max
+bigwigAverage -b E13_Bicalutamide120h_1.bigWig --scaleFactors 0.66 -o E13_Bicalutamide120h_1.bw --numberOfProcessors max
+bigwigAverage -b E13_Bicalutamide5h_1.bigWig --scaleFactors 0.66 -o E13_Bicalutamide5h_1.bw --numberOfProcessors max
+bigwigAverage -b E13_CX5h_1.bigWig --scaleFactors 0.66 -o E13_CX5h_1.bw --numberOfProcessors max
+bigwigAverage -b E13_DHT120h_1.bigWig --scaleFactors 0.66 -o E13_DHT120h_1.bw --numberOfProcessors max
+bigwigAverage -b E13_DHT5h_1.bigWig --scaleFactors 0.66 -o E13_DHT5h_1.bw --numberOfProcessors max
+bigwigAverage -b E13_DHTBicalutamide120h_1.bigWig --scaleFactors 0.66 -o E13_DHTBicalutamide120h_1.bw --numberOfProcessors max
+bigwigAverage -b E13_DHTBicalutamide5h_1.bigWig --scaleFactors 0.66 -o E13_DHTBicalutamide5h_1.bw --numberOfProcessors max
+bigwigAverage -b E14_1_1.bigWig --scaleFactors 1.5 -o E14_1_1.bw --numberOfProcessors max
+bigwigAverage -b E14_FOSL1_1_1.bigWig --scaleFactors 1.5 -o E14_FOSL1_1_1.bw --numberOfProcessors max
+bigwigAverage -b E14_TAZ_1_1.bigWig --scaleFactors 1.5 -o E14_TAZ_1_1.bw --numberOfProcessors max
+bigwigAverage -b E14_TAZ_2_1.bigWig --scaleFactors 1.5 -o E14_TAZ_2_1.bw --numberOfProcessors max
+bigwigAverage -b E14_TAZ_FOSL1_1_1.bigWig --scaleFactors 1.5 -o E14_TAZ_FOSL1_1_1.bw --numberOfProcessors max
+bigwigAverage -b E14_YAP1_1_1.bigWig --scaleFactors 1.5 -o E14_YAP1_1_1.bw --numberOfProcessors max
+bigwigAverage -b E14_YAP1_2_1.bigWig --scaleFactors 1.5 -o E14_YAP1_2_1.bw --numberOfProcessors max
+bigwigAverage -b E14_YAP1_FOSL1_1_1.bigWig --scaleFactors 1.5 -o E14_YAP1_FOSL1_1_1.bw --numberOfProcessors max
+bigwigAverage -b E14_YAP1_FOSL1_2_1.bigWig --scaleFactors 1.5 -o E14_YAP1_FOSL1_2_1.bw --numberOfProcessors max
+bigwigAverage -b E16_1_1.bigWig --scaleFactors 1 -o E16_1_1.bw --numberOfProcessors max
+bigwigAverage -b E16_2_1.bigWig --scaleFactors 1 -o E16_2_1.bw --numberOfProcessors max
+bigwigAverage -b E16_CX_16h_2_1.bigWig --scaleFactors 1 -o E16_CX_16h_2_1.bw --numberOfProcessors max
+
+
+bigwigAverage -b E18_1_1.bigWig --scaleFactors 1.5 -o E18_1_1.bw --numberOfProcessors max
+bigwigAverage -b E18_2_1.bigWig --scaleFactors 1.5 -o E18_2_1.bw --numberOfProcessors max
+bigwigAverage -b E18_3_1.bigWig --scaleFactors 1.5 -o E18_3_1.bw --numberOfProcessors max
+bigwigAverage -b E18_UT143_1_1.bigWig --scaleFactors 1.5 -o E18_UT143_1_1.bw --numberOfProcessors max
+bigwigAverage -b E18_UT143_2_1.bigWig --scaleFactors 1.5 -o E18_UT143_2_1.bw --numberOfProcessors max
+bigwigAverage -b E18_UT143_3_1.bigWig --scaleFactors 1.5 -o E18_UT143_3_1.bw --numberOfProcessors max
+
+bigwigAverage -b E21_abl_1_1.bigWig --scaleFactors 1 -o E21_abl_1_1.bw --numberOfProcessors max
+bigwigAverage -b E21_abl_2_1.bigWig --scaleFactors 1 -o E21_abl_2_1.bw --numberOfProcessors max
+bigwigAverage -b E21_abl_Backbone12h_1_1.bigWig --scaleFactors 1 -o E21_abl_Backbone12h_1_1.bw --numberOfProcessors max
+bigwigAverage -b E21_abl_Backbone12h_2_1.bigWig --scaleFactors 1 -o E21_abl_Backbone12h_2_1.bw --numberOfProcessors max
+bigwigAverage -b E21_abl_DHT12h_1_1.bigWig --scaleFactors 1 -o E21_abl_DHT12h_1_1.bw --numberOfProcessors max
+bigwigAverage -b E21_abl_DHT12h_2_1.bigWig --scaleFactors 1 -o E21_abl_DHT12h_2_1.bw --numberOfProcessors max
+bigwigAverage -b E21_abl_ENZ12h_1_1.bigWig --scaleFactors 1 -o E21_abl_ENZ12h_1_1.bw --numberOfProcessors max
+bigwigAverage -b E21_abl_ENZ12h_2_1.bigWig --scaleFactors 1 -o E21_abl_ENZ12h_2_1.bw --numberOfProcessors max
+bigwigAverage -b E21_abl_MPC30912h_1_1.bigWig --scaleFactors 1 -o E21_abl_MPC30912h_1_1.bw --numberOfProcessors max
+bigwigAverage -b E21_abl_MPC30912h_2_1.bigWig --scaleFactors 1 -o E21_abl_MPC30912h_2_1.bw --numberOfProcessors max
+bigwigAverage -b E22_1_1.bigWig --scaleFactors 2 -o E22_1_1.bw --numberOfProcessors max
+bigwigAverage -b E22_2_1.bigWig --scaleFactors 2 -o E22_2_1.bw --numberOfProcessors max
+bigwigAverage -b E22_3_1.bigWig --scaleFactors 2 -o E22_3_1.bw --numberOfProcessors max
+bigwigAverage -b E22_4_1.bigWig --scaleFactors 2 -o E22_4_1.bw --numberOfProcessors max
+bigwigAverage -b E22_ENZ168h_1_1.bigWig --scaleFactors 2 -o E22_ENZ168h_1_1.bw --numberOfProcessors max
+bigwigAverage -b E22_ENZ168h_2_1.bigWig --scaleFactors 2 -o E22_ENZ168h_2_1.bw --numberOfProcessors max
+bigwigAverage -b E22_ENZ168h_OC2i_1_1.bigWig --scaleFactors 2 -o E22_ENZ168h_OC2i_1_1.bw --numberOfProcessors max
+bigwigAverage -b E22_ENZ168h_OC2i_2_1.bigWig --scaleFactors 2 -o E22_ENZ168h_OC2i_2_1.bw --numberOfProcessors max
+bigwigAverage -b E22_OC2_1_1.bigWig --scaleFactors 2 -o E22_OC2_1_1.bw --numberOfProcessors max
+bigwigAverage -b E22_OC2_2_1.bigWig --scaleFactors 2 -o E22_OC2_2_1.bw --numberOfProcessors max
+
+bigwigAverage -b E3_omniATAC_1_1.bigWig --scaleFactors 1 -o E3_omniATAC_1_1.bw --numberOfProcessors max
+bigwigAverage -b E3_omniATAC_2_1.bigWig --scaleFactors 1 -o E3_omniATAC_2_1.bw --numberOfProcessors max
+bigwigAverage -b E5_1_1.bigWig --scaleFactors 1 -o E5_1_1.bw --numberOfProcessors max
+bigwigAverage -b E5_2_1.bigWig --scaleFactors 1 -o E5_2_1.bw --numberOfProcessors max
+bigwigAverage -b E5_LSD1i_1_1.bigWig --scaleFactors 1 -o E5_LSD1i_1_1.bw --numberOfProcessors max
+bigwigAverage -b E5_LSD1i_2_1.bigWig --scaleFactors 1 -o E5_LSD1i_2_1.bw --numberOfProcessors max
+
+bigwigAverage -b E6_1_1.bigWig --scaleFactors 2 -o E6_1_1.bw --numberOfProcessors max
+bigwigAverage -b E6_2_1.bigWig --scaleFactors 2 -o E6_2_1.bw --numberOfProcessors max
+bigwigAverage -b E6_ENZ120h_1_1.bigWig --scaleFactors 2 -o E6_ENZ120h_1_1.bw --numberOfProcessors max
+bigwigAverage -b E6_ENZ120h_2_1.bigWig --scaleFactors 2 -o E6_ENZ120h_2_1.bw --numberOfProcessors max
+bigwigAverage -b E6_ENZ168h_1_1.bigWig --scaleFactors 2 -o E6_ENZ168h_1_1.bw --numberOfProcessors max
+bigwigAverage -b E6_ENZ168h_2_1.bigWig --scaleFactors 2 -o E6_ENZ168h_2_1.bw --numberOfProcessors max
+bigwigAverage -b E6_ENZ24h_1_1.bigWig --scaleFactors 2 -o E6_ENZ24h_1_1.bw --numberOfProcessors max
+bigwigAverage -b E6_ENZ24h_2_1.bigWig --scaleFactors 2 -o E6_ENZ24h_2_1.bw --numberOfProcessors max
+bigwigAverage -b E6_ENZ336h_1_1.bigWig --scaleFactors 2 -o E6_ENZ336h_1_1.bw --numberOfProcessors max
+bigwigAverage -b E6_ENZ336h_2_1.bigWig --scaleFactors 2 -o E6_ENZ336h_2_1.bw --numberOfProcessors max
+bigwigAverage -b E6_ENZ72h_1_1.bigWig --scaleFactors 2 -o E6_ENZ72h_1_1  Androgen10.bw --numberOfProcessors max
+bigwigAverage -b E6_ENZ72h_2_1.bigWig --scaleFactors 2 -o E6_ENZ72h_2_1  Androgen10.bw --numberOfProcessors max
